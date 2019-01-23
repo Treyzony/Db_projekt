fa8d1988acd6c69c9da950b69e38a2021961ca1c
jtissen1 <jakob.tissen@fh-bielefeld.de>
2019-01-23 01:06:23 +0100

    kleiner datensatz erweitert

--- SqlSkipte/cards_in_sets.sql
+++ SqlSkipte/cards_in_sets.sql
@@ -1,5 +1,42 @@
-INSERT INTO Cards_In_Set (card_id, set_id) VALUES (
-	SELECT Card2.Nid Card_Set.set_id
-	FROM Card2 LEFT JOIN Card_Set Card2.Nset = Card_Set.short
-	WHERE Card2.Nset != NULL
-);
+INSERT INTO Cards_In_Set (card_id, set_id) 
+SELECT Card2.Nid, Card_Set.set_id
+FROM Card2 LEFT JOIN Card_Set on Card2.Nset = Card_Set.short
+;
+
+INSERT INTO card(
+card_id ,
+name ,
+	converted_mana_cost ,
+	typename ,
+	powercnt ,
+	toughness ,
+	rules ,
+	rarity ,
+	card_color ,
+	artistname ,
+	name_CN ,
+	name_TW ,
+	name_FR ,
+	name_DE ,
+	name_IT ,
+	name_JP ,
+	name_PT ,
+	name_RU ,
+	name_ES ,
+	name_KO ,
+	type_CN ,
+	type_TW ,
+	type_FR ,
+	type_DE ,
+	type_IT ,
+	type_JP ,
+	type_PT ,
+	type_RU ,
+	type_ES ,
+	type_KO )
+SELECT card2.Nid,
+card2.Nname,card2.Nconverted_manacost,card2.Ntype,card2.Npower,card2.Ntoughness,card2.Nruling,card2.Nrarity,
+card2.Ncolor,card2.Nartist,
+card2.Nname_CN,card2.Nname_TW,card2.Nname_FR,card2.Nname_DE,card2.Nname_IT,card2.Nname_JP,card2.Nname_PT,card2.Nname_RU,card2.Nname_ES,
+card2.Nname_KO,card2.Ntype_CN,card2.Ntype_TW,card2.Ntype_FR,card2.Ntype_DE,card2.Ntype_IT,card2.Ntype_JP,card2.Ntype_PT,card2.Ntype_RU,card2.Ntype_ES,card2.Ntype_KO
+from card2;
